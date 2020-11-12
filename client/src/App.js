58a7260c32fda0ac0f844a60c6868b26c37f9584
vpetridis <vasileios.petridis@gmail.com>
2020-11-12 20:19:36 +0200

    created lots of stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import React, { Fragment } from "react";
 import "./App.css";
+import EditTodo from "./components/EditTodo";
 
 //components
+import { Container, Col, Row } from "react-bootstrap";
 
 import InputTodo from "./components/InputTodo";
 
 function App() {
   return (
     <Fragment>
-      <div className="container">
-        <InputTodo />
-      </div>
+      <h1 className="text-center">Pern Todo List</h1>
+      <Container fluid className="m-3 justify-content-cente">
+        <Row>
+          <Col>
+            {" "}
+            <InputTodo />
+          </Col>{" "}
+          <Col xs={3}>
+            {" "}
+            <EditTodo />
+          </Col>
+        </Row>
+      </Container>
     </Fragment>
   );
 }
--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -21,8 +21,7 @@ console.log(response);
 
   return (
     <Fragment>
-      <h1 className="text-center mt-5">Pern Todo List</h1>
-      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+      <form className="d-flex" onSubmit={onSubmitForm}>
         <input
           type="text"
           className="form-control"
