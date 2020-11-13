e6413fd66d43d361be088c080769b74328d41187
vpetridis <vasileios.petridis@gmail.com>
2020-11-13 19:12:24 +0200

    bit install bounce-in

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -3,15 +3,15 @@ import React, { Component } from "react";
 export default class ListTodo extends Component {
   constructor() {
     super();
-    this.state = { description: [] };
+    this.state = { todo: [] };
   }
 
   componentDidMount() {
     fetch(`http://localhost:5000/todos`)
       .then((res) => res.json())
-      .then((result) => {console.log(result);});
+      .then((result) => this.setState({todo: result.description}));
   }
   render() {
-    return <div>Description: {this.state.description}</div>;
+    return <div>Description: {this.state.todo}</div>;
   }
 }
