4551bed54eba1fb73c6f2fbff4c088641a333665
vpetridis <vasileios.petridis@gmail.com>
2020-11-14 13:16:24 +0200

    created card in ListTodo.js

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,4 +1,13 @@
 import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
 
 export default class ListTodo extends Component {
   state = { description: [] };
@@ -9,15 +18,29 @@ export default class ListTodo extends Component {
       .then((result) => this.setState({ description: result }));
   }
   render() {
-
     return (
       <div>
-        <ul>
-          {this.state.description.map((todo) => (
-              <li className="Wrapper" key={todo.todo_id}>{todo.description}</li>
-          ))}
-        </ul>
+        {this.state.description.map((todo) => (
+          <Card>
+            <CardBody>
+              <CardTitle tag="h5">Todo</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">
+                Todo description:{" "}
+              </CardSubtitle>
+              <CardText>{todo.description}</CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        ))}
       </div>
     );
   }
 }
+
+// <div>
+//   <ul>
+//     {this.state.description.map((todo) => (
+//         <li className="Wrapper" key={todo.todo_id}>{todo.description}</li>
+//     ))}
+//   </ul>
+// </div>
