481d25f422c3174cfcd085c85dad52d49726c781
vpetridis <vasileios.petridis@gmail.com>
2020-11-14 15:46:26 +0200

    deleteTodo didnt work

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,26 +1,42 @@
 import React, { Component } from "react";
 import {
   Card,
-  CardImg,
+  Container,
+  Row,
   CardText,
   CardBody,
   CardTitle,
   CardSubtitle,
   Button,
+  Spinner,
 } from "reactstrap";
 
 export default class ListTodo extends Component {
-  state = { todo_id: [], description: [] };
+  state = { todo: [{ todo_id: 0, description: "" }], isLoading: false };
 
   componentDidMount() {
+    this.setState({ isLoading: true });
     fetch("http://localhost:5000/todos")
       .then((res) => res.json())
-      .then((result) => this.setState({todo_id: result, description: result }, console.log(result)));
+      .then((result) =>
+        this.setState({ todo: result, isLoading: false }, console.log(result))
+      );
   }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <Container>
+          <Row className="centered">
+            {" "}
+            <Spinner color="primary" />
+          </Row>
+        </Container>
+      );
+    }
     return (
       <div>
-        {this.state.description.map((todo) => (
+        {this.state.todo.map((todo) => (
           <Card>
             <CardBody>
               <CardTitle tag="h5">Todo ID: {todo.todo_id}</CardTitle>
@@ -31,7 +47,7 @@ export default class ListTodo extends Component {
               <Button color="warning" className="mr-4">
                 Edit
               </Button>
-              <Button color="danger">Delete</Button>
+              {/* <Button color="danger" onClick={deleteMethod(todo.todo_id)}>Delete</Button> */}
             </CardBody>
           </Card>
         ))}
@@ -40,10 +56,10 @@ export default class ListTodo extends Component {
   }
 }
 
-// <div>
-//   <ul>
-//     {this.state.description.map((todo) => (
-//         <li className="Wrapper" key={todo.todo_id}>{todo.description}</li>
-//     ))}
-//   </ul>
-// </div>
+// // TODO - delete function
+// function deleteMethod(props) {
+//   const deleteTodo = {
+//     method: "DELETE"};
+//   fetch(`http://localhost:5000/todos/${props}`, deleteTodo)
+//     .then((res) => res.json());
+// }
--- server/index.js
+++ server/index.js
@@ -30,7 +30,6 @@ app.get("/todos", async (req, res) => {
   try {
     const allTodos = await pool.query("SELECT * FROM todo");
     res.json(allTodos.rows);
-    console.log(allTodos.rows);
   } catch (err) {
     console.error(err.message);
   }
