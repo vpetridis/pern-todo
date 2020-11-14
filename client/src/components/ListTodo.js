b3360d9c3b5065137463998bf69a8cca3a99ac02
vpetridis <vasileios.petridis@gmail.com>
2020-11-14 10:15:33 +0200

    CREATED MANY MODULES!

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 
 export default class ListTodo extends Component {
-  constructor() {
-    super();
-    this.state = { todo: [] };
-  }
+  state = { description: [] };
 
   componentDidMount() {
-    fetch(`http://localhost:5000/todos`)
+    fetch("http://localhost:5000/todos")
       .then((res) => res.json())
-      .then((result) => this.setState({todo: result.description}));
+      .then((result) => this.setState({ description: result }));
   }
   render() {
-    return <div>Description: {this.state.todo}</div>;
+    return (
+      <div>
+        <ul>
+          <div>
+            {this.state.description.map((todo) => (
+              <li key={todo.todo_id}>{todo.description}</li>
+            ))}
+          </div>
+        </ul>
+      </div>
+    );
   }
 }
